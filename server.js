1e08bf2f04a4526738198c236d80c7dd05a2bdd9
Charlie Liu <1103107109@gm.kuas.edu.tw>
2017-07-25 12:14:18 +0000

    test

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');  
- 
+var body_parser = require('body-parser');
 var app = express();  
+
+app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.json());
+
 app.set('view engine', 'ejs');  
-app.use(express.static('public'));
+
 app.get('/', function(req, res) {
  res.render('index', {
    id: ['index', 'post_form', 'get_form'],
@@ -18,7 +22,7 @@ app.get('/post_form', function(req, res) {
    style: ['', 'active', ''],
    li: ['首頁', 'POST', 'GET'],
    href: ['/', 'post_form', 'get_form'],
-   content: '<form action="post_test" method="post">POST:<input type="text" name="something"/><input type="submit" value="Submit"/></form>'
+   content: '<form action="post_action" method="post">POST:<input type="text" name="something"/><input type="submit" value="Submit"/></form>'
   });
 });
 app.get('/get_form', function(req, res) {
@@ -27,7 +31,27 @@ app.get('/get_form', function(req, res) {
    style: ['', '', 'active'],
    li: ['首頁', 'POST', 'GET'],
    href: ['/', 'post_form', 'get_form'],
-   content: '<form action="get_test" method="get">GET:<input type="text" name="something"/><input type="submit" value="Submit"/></form>'
+   content: '<form action="get_action" method="get">GET:<input type="text" name="something"/><input type="submit" value="Submit"/></form>'
+  });
+});
+app.post('/post_action', function(req, res) {
+    var post_msg = req.body.something;
+ res.render('index', {
+   id: ['index', 'post_form', 'get_form'],
+   style: ['', '', ''],
+   li: ['首頁', 'POST', 'GET'],
+   href: ['/', 'post_form', 'get_form'],
+   content: 'POST得到：'+post_msg
+  });
+});
+app.get('/get_action', function(req, res) {
+    var get_msg = req.query.something;
+ res.render('index', {
+   id: ['index', 'post_form', 'get_form'],
+   style: ['', '', ''],
+   li: ['首頁', 'POST', 'GET'],
+   href: ['/', 'post_form', 'get_form'],
+   content: 'GET得到：'+get_msg
   });
 });
 app.get(/(.*)(\.)*/i, function(req, res) {  
