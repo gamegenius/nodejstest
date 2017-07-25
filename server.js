46be440274357662e212e2f310a23c7175327bb5
Charlie Liu <1103107109@gm.kuas.edu.tw>
2017-07-25 12:29:23 +0000

    test

--- server.js
+++ server.js
@@ -1,58 +1,43 @@
 var express = require('express');  
 var body_parser = require('body-parser');
 var app = express();  
-
+var msg = {
+   "id": ['index', 'post_form', 'get_form'],
+   "css": ['', '', ''],
+   "li": ['首頁', 'POST', 'GET'],
+   "href": ['/', 'post_form', 'get_form'],
+   "content": ''
+  };
+  console.log(msg.id);
 app.use(body_parser.urlencoded({ extended: false }));
 app.use(body_parser.json());
 
 app.set('view engine', 'ejs');  
 
 app.get('/', function(req, res) {
- res.render('index', {
-   id: ['index', 'post_form', 'get_form'],
-   style: ['active', '', ''],
-   li: ['首頁', 'POST', 'GET'],
-   href: ['/', 'post_form', 'get_form'],
-   content: '<h1>Hello world~中文?</h1>'
-  });
+    msg.css = ['active', '', ''];
+    msg.content= '<h1>Hello world~中文?</h1>';
+ res.render('index', msg);
 });
 app.get('/post_form', function(req, res) {
- res.render('index', {
-   id: ['index', 'post_form', 'get_form'],
-   style: ['', 'active', ''],
-   li: ['首頁', 'POST', 'GET'],
-   href: ['/', 'post_form', 'get_form'],
-   content: '<form action="post_action" method="post">POST:<input type="text" name="something"/><input type="submit" value="Submit"/></form>'
-  });
+    msg.css= ['', 'active', ''];
+   msg.content= '<form action="post_action" method="post">POST:<input type="text" name="something"/><input type="submit" value="Submit"/></form>';
+ res.render('index', msg);
 });
 app.get('/get_form', function(req, res) {
- res.render('index', {
-   id: ['index', 'post_form', 'get_form'],
-   style: ['', '', 'active'],
-   li: ['首頁', 'POST', 'GET'],
-   href: ['/', 'post_form', 'get_form'],
-   content: '<form action="get_action" method="get">GET:<input type="text" name="something"/><input type="submit" value="Submit"/></form>'
-  });
+    msg.css= ['', '', 'active'];
+   msg.content= '<form action="get_action" method="get">GET:<input type="text" name="something"/><input type="submit" value="Submit"/></form>';
+ res.render('index', msg);
 });
 app.post('/post_action', function(req, res) {
     var post_msg = req.body.something;
- res.render('index', {
-   id: ['index', 'post_form', 'get_form'],
-   style: ['', '', ''],
-   li: ['首頁', 'POST', 'GET'],
-   href: ['/', 'post_form', 'get_form'],
-   content: 'POST得到：'+post_msg
-  });
+    msg.content = 'POST得到：'+post_msg;
+ res.render('index', msg);
 });
 app.get('/get_action', function(req, res) {
     var get_msg = req.query.something;
- res.render('index', {
-   id: ['index', 'post_form', 'get_form'],
-   style: ['', '', ''],
-   li: ['首頁', 'POST', 'GET'],
-   href: ['/', 'post_form', 'get_form'],
-   content: 'GET得到：'+get_msg
-  });
+    msg.content = 'GET得到：'+get_msg;
+ res.render('index', msg);
 });
 app.get(/(.*)(\.)*/i, function(req, res) {  
   res.sendfile(__dirname + "/" + req.params[0], function(err) {
